095e7922fc67979355bb0b6c3a0ff8364d3959a2
IlyasAmirat <i.amirat@esi-sba.dz>
2021-08-18 11:54:48 +0100

    api-gateway-service init

---
+++ api-gateway-service/src/main/java/dz/esi/apigateway/ApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package dz.esi.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayServiceApplication.class, args);
+    }
+
+}
