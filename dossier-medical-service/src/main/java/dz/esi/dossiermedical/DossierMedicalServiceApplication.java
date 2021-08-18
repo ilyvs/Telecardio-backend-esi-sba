07c3e3e565a22f7a904581362edd9321469f765c
IlyasAmirat <i.amirat@esi-sba.dz>
2021-08-18 11:30:08 +0100

    dossier-medical-service init

---
+++ dossier-medical-service/src/main/java/dz/esi/dossiermedical/DossierMedicalServiceApplication.java
@@ -0,0 +1,13 @@
+package dz.esi.dossiermedical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DossierMedicalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DossierMedicalServiceApplication.class, args);
+    }
+
+}
