1f2341340586ffdca0b44eb6d2ea20d580381c71
IlyasAmirat <i.amirat@esi-sba.dz>
2021-08-23 14:32:13 +0100

    Add FallBackPost

--- api-gateway-service/src/main/java/dz/esi/apigateway/controller/CircuitBreakerController.java
+++ api-gateway-service/src/main/java/dz/esi/apigateway/controller/CircuitBreakerController.java
@@ -1,6 +1,7 @@
 package dz.esi.apigateway.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +13,32 @@ public class CircuitBreakerController {
         return " Dossier Medical Service Goes Down";
     }
 
+    @PostMapping("/dossierMedicalServiceDefaultResponse")
+    public String dossierMedicalServiceFallBackPost() {
+        return " Dossier Medical Service Goes Down";
+    }
+
+    
+
     @GetMapping("/examenCliniqueServiceDefaultResponse")
     public String examenCliniqueServiceFallBack() {
         return " Examen Clinique Service Goes Down";
     }
 
+    @PostMapping("/examenCliniqueServiceDefaultResponse")
+    public String examenCliniqueServiceFallBackPost() {
+        return " Examen Clinique Service Goes Down";
+    }
+
+
+
     @GetMapping("/gestionCompteServiceDefaultResponse")
     public String gestionCompteServiceFallBack() {
+        return "Gestion Compte Service Goes Down";
+    }
+
+    @PostMapping("/gestionCompteServiceDefaultResponse")
+    public String gestionCompteServiceFallBackPost() {
         return "Gestion Compte Service Goes Down";
     }
 
