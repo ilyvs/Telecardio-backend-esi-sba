049aa45dc8d7a9eecb810d3365f82cf2238821bf
IlyasAmirat <i.amirat@esi-sba.dz>
2021-09-03 11:26:51 +0100

    ECG service init

---
+++ ecg-service/src/main/java/dz/esi/ecg/EcgServiceApplication.java
@@ -0,0 +1,13 @@
+package dz.esi.ecg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcgServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcgServiceApplication.class, args);
+    }
+
+}
---
+++ ecg-service/src/test/java/dz/esi/ecg/EcgServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dz.esi.ecg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcgServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
