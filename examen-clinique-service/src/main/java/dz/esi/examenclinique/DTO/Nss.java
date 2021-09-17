8e009006e2ac2a43a6b843b9e691b0dd8782ef21
IlyasAmirat <i.amirat@esi-sba.dz>
2021-09-17 21:22:22 +0100

    fix clinique service

--- examen-clinique-service/src/main/java/dz/esi/examenclinique/DTO/Nss.java
+++ examen-clinique-service/src/main/java/dz/esi/examenclinique/DTO/Nss.java
@@ -4,14 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Nss {
 
     @Id
